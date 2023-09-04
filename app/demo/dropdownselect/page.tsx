b37ff0fb1019ca9d83026c2b3828b87e245203dc
Katy MDC <katymdc@gmail.com>
2023-09-04 01:17:43 -0600

    added source code url

--- app/demo/dropdownselect/page.tsx
+++ app/demo/dropdownselect/page.tsx
@@ -16,7 +16,7 @@ const DropdownDemo = () => {
     return (
         <main className="min-h-screen p-4">
             <h1 className="mb-4 text-lg">DropdownSelect Reusable React Component Demo</h1>
-            <p className="mb-4"><a className="underline text-blue-600" href="">Git Source Code</a></p>
+            <p className="mb-4"><a href="https://github.com/katymdc/portfolio-site" className="underline text-blue-600" href="">Git Source Code</a></p>
             <p className="mb-1">Technologies Used</p>
             <ul className="mb-8 ml-8 list-disc">
                 <li>ReactJS</li>
